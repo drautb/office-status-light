3efe978dc3f7045996937d7303cf662a7236e1cb
Ben Draut <drautb@gmail.com>
2022-03-10 11:22:50 -0700

    Add script to update status

---
+++ update-status.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+
+STATUS="$1"
+
+DEFAULT_HOST="rpi-zero-light.local"
+HOST="${2:-$DEFAULT_HOST}"
+
+r=0
+g=0
+b=0
+
+if [ "$STATUS" = "available" ]; then  # Green
+  g=255
+elif [ "$STATUS" = "focus" ]; then    # Yellow
+  r=255
+  g=255
+elif [ "$STATUS" = "meeting" ]; then  # Red
+  r=255
+else
+  printf "Unreocgnized staus: '%s', turning off light...\n", "$STATUS"
+fi
+
+curl -X PUT -H "Content-Type: application/json" "$HOST:5000/color" -d "{\"r\":$r,\"g\":$g,\"b\":$b}"
+
