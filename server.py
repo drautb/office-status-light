0daf6b26506c67148724d1f4e972080ae13fab42
Ben Draut <drautb@gmail.com>
2022-03-29 18:50:31 -0600

    Fix brightness

--- server.py
+++ server.py
@@ -49,7 +49,7 @@ def get_color():
 
 @app.route("/brightness", methods=["GET"])
 def get_brightness():
-    return current_color
+    return current_brightness
 
 app.run(host="0.0.0.0", debug=False)
 
